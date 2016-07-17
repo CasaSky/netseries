a9b063fc8edac8f41bd9368d9ff969c9a1c0c054
Talal Tabia <talal.tabia@haw-hamburg.de>
2016-07-17 18:39:07 +0200

    Factory

---
+++ src/main/java/de/netseries/entities/Medium.java
@@ -0,0 +1,7 @@
+package de.netseries.entities;
+
+/**
+ * Created by talal on 17.07.16.
+ */
+public abstract class Media extends Entity{
+}
---
+++ src/main/java/de/netseries/utils/Kategorie.java
@@ -0,0 +1,8 @@
+package de.netseries.entities;
+
+/**
+ * Created by talal on 17.07.16.
+ */
+public enum Kategorie {
+    ACTION, ADVENTURE, COMEDY, DRAMA, MARTIALARTS, MYSTERY, ROMANCE, THRILLER, WESTERN, CRIME, SPORTS, SCIFY;
+}
